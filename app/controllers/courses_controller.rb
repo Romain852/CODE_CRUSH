e48fd2b87fbc9383078beafda46848413fd6ea80
Seha Sahin <sahinseha@gmail.com>
2023-04-11 20:03:38 +1000

    added courses controller, implemented crud actions

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,43 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: %i[show edit update destroy]
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.user = current_user
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :price, :syllabus)
+  end
+end
