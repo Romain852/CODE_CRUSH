7fdec4c13d9c0d28280efffee37687a71a150c7e
Seha Sahin <sahinseha@gmail.com>
2023-04-12 11:58:48 +1000

    added route for enrolments for the signed-in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :courses
+  get "users/:id/enrolments", to: "enrolments#index"
 end
