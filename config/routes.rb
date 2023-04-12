8da4c76a260488906741b72dc28a3a472745b11b
Seha Sahin <sahinseha@gmail.com>
2023-04-12 15:14:41 +1000

    added enrol buttons to navbar, courses index and show pages, implemented enrolments indexand create actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :courses
+  # Show enrolments for currently signed-in user
   get "users/:id/enrolments", to: "enrolments#index", as: :enrolments
+  # Enrol in course for currently signed-in user
+  get "courses/:id/enrol", to: "enrolments#create", as: :enrol_new
 end
