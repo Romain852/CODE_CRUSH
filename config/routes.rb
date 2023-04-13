4766920f474da106e6ec28c0d1c6b60af471a441
Romain Herbreteau <r.herbreteau@live.fr>
2023-04-13 19:26:58 +0800

    set up category page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "pages#home"
-  resources :courses
+
   # Show enrolments for currently signed-in user
   get "users/:id/enrolments", to: "enrolments#index", as: :enrolments
   # Enrol in course for currently signed-in user
   get "courses/:id/enrol", to: "enrolments#create", as: :enrol_new
+
+  resources :courses do
+    collection do
+      get :fullstack
+      get :data
+    end
+  end
 end
