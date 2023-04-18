bc84e7886481965cf2f5b3aabdcd87c46c47ca21
Seha Sahin <sahinseha@gmail.com>
2023-04-18 19:09:17 +1000

    schema file copied from you guys

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_045755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -23,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
-
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -35,13 +32,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -52,10 +47,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
+    t.datetime "session_start"
+    t.datetime "session_end"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
-
   create_table "enrolments", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "course_id", null: false
@@ -65,7 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
     t.index ["course_id"], name: "index_enrolments_on_course_id"
     t.index ["user_id"], name: "index_enrolments_on_user_id"
   end
-
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "comment"
@@ -76,7 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
     t.index ["enrolment_id"], name: "index_reviews_on_enrolment_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -94,7 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "courses", "users"
