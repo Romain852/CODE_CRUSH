9747409bfd3687ff35129f79b5046c4b23f7e8ed
Seha Sahin <sahinseha@gmail.com>
2023-04-18 16:58:38 +1000

    seed the DB

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem "jbuilder"
 
 gem "devise"
 
+gem "faker"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+courses = [
+          "Ruby",
+          "CSS",
+          "JavaScript",
+          "Databases",
+          "Communication",
+          "Product Management",
+          "DevOps",
+          "Entrepreneurship",
+          "HTML",
+          "UX Design",
+          "Data Science",
+          "Python",
+          "Rails"
+        ]
+
+courses.each do |course|
+  Course.create(
+    title: course,
+    description: Faker::Lorem.paragraph(sentence_count: 4),
+    price: rand(500..2500),
+    category: ["Fullstack", "Data"].sample,
+    syllabus: Faker::Lorem.paragraph(sentence_count: 50),
+    user: User.all.sample,
+    session_start: Time.now,
+    session_end: Time.now + 14.days
+  )
+end
