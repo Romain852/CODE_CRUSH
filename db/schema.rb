cc0fdf89cc22e84c5bb1763cb6a67a98571ea4ea
Mathew Hocking <matt8443478@gmail.com>
2023-04-18 19:21:23 +1000

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_045755) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_105917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_045755) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.datetime "session_start"
     t.datetime "session_end"
     t.index ["user_id"], name: "index_courses_on_user_id"
